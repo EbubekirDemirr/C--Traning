02aeb9ae79a89b23d4625f58946bff916364ed35
Ebubekir Demir <qeqw12@outlook.com>
2022-12-25 14:26:44 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //bir class'ı kullanabilmek için onun bir örneğini oluşturmamız gereklidir. 
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add();
+            customerManager.Update();
+
+            ProductManager productManager= new ProductManager();
+            productManager.Add();
+            productManager.Update();
+
+            Customer customer = new Customer();
+            customer.FirstName = "EBUBEKİR";
+            customer.LastName = "DEMİR";
+            customer.Id = 1;
+            customer.City = "ORDU";
+
+            Customer customer2 = new Customer
+            {
+                City = "GAZİANTEP",
+                Id = 1,
+                FirstName = "KAĞAN",
+                LastName = "DOĞAN"
+            };
+
+            Console.WriteLine(customer.FirstName);
+            Console.ReadLine();
+        }
+    }
+    
+   
+
+}
